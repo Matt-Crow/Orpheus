415d86bca462d268f1297c02126133cb19d58a4e
IronHeart7334 <greengrappler12@gmail.com>
2017-05-24 09:08:08 -0700

    Worked on EasyButton
    need actions, implement

---
+++ src/resources/EasyButton.java
@@ -0,0 +1,19 @@
+package resources;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class EasyButton extends JButton{
+	public EasyButton(String text, int x, int y, int w, int h, Color c){
+		super(text);
+		setLayout(null);
+		setOpaque(true);
+		setBorderPainted(false);
+		setBounds(x, y, w, h);
+		setBackground(c);
+	}
+	public void addTo(JPanel j){
+		j.add(this);
+	}
+}
--- src/windows/MainCanvas.java
+++ src/windows/MainCanvas.java
@@ -2,18 +2,35 @@ package windows;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.AbstractAction;
 
-import menus.Menu;
-
+@SuppressWarnings("serial")
 public class MainCanvas extends JPanel{
 	public static final long serialVersionUID = 1L;
-	
+	JPanel p = this;
 	public MainCanvas(){
 		setLayout(null);
 		setBackground(Color.black);
-		Menu m = new Menu("Hi", 0, 0, 100, 100);
-		m.set(this);
+		
+		JButton quit = new JButton(new AbstractAction("Quit"){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				new StartWindow();
+				JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(p);
+				frame.dispose();
+			}
+		});
+		quit.setLayout(null);
+		quit.setOpaque(true);
+		quit.setBorderPainted(false);
+		quit.setBounds(0, 0, 100, 100);
+		quit.setBackground(Color.red);
+		add(quit);
 	}
 	
 	public void paintComponent(Graphics g){
