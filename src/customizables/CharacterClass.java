b9d459d3685f05f4f4c50d29146338b35b22dcd0
IronHeart7334 <greengrappler12@gmail.com>
2017-05-28 15:21:39 -0700

    lotta work

--- src/customizables/CharacterClass.java
+++ src/customizables/CharacterClass.java
@@ -1,5 +1,21 @@
 package customizables;
+import upgradables.Stat;
 
 public class CharacterClass {
-
+	private Stat baseHP;
+	private Stat baseRegenPerc;
+	private Stat baseRegenWait;
+	
+	public void setHPData(double HP, double regen, double wait){
+		baseHP = new Stat("HP", 500 * HP, 2);
+		baseRegenPerc = new Stat("Healing", 5 * regen);
+		baseRegenWait = new Stat("Heal rate", 20 * wait);
+	}
+	public void calcStats(){
+		
+	}
+	
+	public void displayHPData(){
+		
+	}
 }
---
+++ src/upgradables/Stat.java
@@ -0,0 +1,47 @@
+package upgradables;
+import resources.Op;
+
+public class Stat {
+	private String name;
+	private double baseValue;
+	private double maxValue;
+	private double step;
+	private int level;
+	private double value;
+	
+	public Stat(String n, double base, double maxRelativeToMin){
+		baseValue = base;
+		maxValue = baseValue * maxRelativeToMin;
+		step = maxValue - base / 10;
+		level = 0;
+	}
+	public Stat(String n, double val){
+		baseValue = val;
+		maxValue = val;
+		step = 0;
+		level = 0;
+	}
+	public void upgrade(){
+		level += 1;
+	}
+	public void setLevel(int lv){
+		level = lv;
+		if(level > 10){
+			level = 10;
+		}
+	}
+	public void calc(){
+		 value = baseValue + step * level;
+	}
+	public void displayData(){
+		Op.add(name);
+		Op.add(baseValue + "-" + maxValue);
+		Op.add(step + " step");
+		Op.add("at level " + level + ":");
+		Op.add(value + " ");
+		Op.dp();
+	}
+	public double get(){
+		return value;
+	}
+}
