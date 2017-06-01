00dcdb9c8bb3867a1354b2734b3d59039c67ea7b
IronHeart7334 <greengrappler12@gmail.com>
2017-06-01 09:32:03 -0700

    Added displayPopup, needs imp

--- src/customizables/CharacterClass.java
+++ src/customizables/CharacterClass.java
@@ -1,16 +1,21 @@
 package customizables;
 import upgradables.Stat;
 import battle.AttackInstance;
+
 import java.util.ArrayList;
 import java.util.Locale;
+import java.awt.Graphics;
+import java.awt.Color;
 
 public class CharacterClass {
 	private ArrayList<Stat> stats;
 	private double damageBacklog;
 	private int remHP;
 	private int energy;
+	public String name;
 	
-	public CharacterClass(){
+	public CharacterClass(String n){
+		name = n;
 		stats = new ArrayList<>();
 	}
 	
@@ -19,6 +24,7 @@ public class CharacterClass {
 		stats.add(new Stat("Healing", 3.75 + 1.25 * regen));
 		stats.add(new Stat("Heal rate", 35 - 5 * wait));
 	}
+	
 	public void setEnergyData(int max, int epr, int er, int eph, int ephr){
 		stats.add(new Stat("Max energy", 12.5 * (max + 1), 2));
 		stats.add(new Stat("EPR", epr, 2));
@@ -26,11 +32,13 @@ public class CharacterClass {
 		stats.add(new Stat("EPH", eph + 2, 2));
 		stats.add(new Stat("EPHR", ephr + 2, 2));
 	}
+	
 	public void calcStats(){
 		for(Stat stat : stats){
 			stat.calc();
 		}
 	}
+	
 	public Stat getStat(String n){
 		for(Stat stat : stats){
 			if(stat.name.toLowerCase(Locale.ENGLISH) == n.toLowerCase(Locale.ENGLISH)){
@@ -39,22 +47,39 @@ public class CharacterClass {
 		}
 		return new Stat("STATNOTFOUND", 0);
 	}
+	
 	public double getStatValue(String n){
 		return getStat(n).get();
 	}
+	
+	public void displayPopup(int x, int y, Graphics g){
+		g.setColor(Color.white);
+		g.fillRect(x, y, 100, 200);
+		g.setColor(Color.black);
+		g.drawString(name, x + 10, y + 10);
+		int statY = y + 30;
+		for(Stat stat : stats){
+			g.drawString(stat.name, x, statY);
+			statY += 20;
+		}
+	}
+	
 	public void displayStatData(){
 		for(Stat stat: stats){
 			stat.displayData();
 		}
 	}
+	
 	public void initForBattle(){
 		calcStats();
 		remHP = (int) getStatValue("maxHP");
 		energy = (int) getStatValue("Max energy");
 	}
+	
 	public void logDamage(AttackInstance attack){
 		damageBacklog += attack.calcDamage();
 	}
+	
 	public void depleteBacklog(){
 		double damage;
 		if(damageBacklog > getStatValue("maxHP")){
--- src/customizables/Earth.java
+++ src/customizables/Earth.java
@@ -2,7 +2,7 @@ package customizables;
 
 public class Earth extends CharacterClass{
 	public Earth(){
-		super();
+		super("Earth");
 		setHPData(4, 2, 3);
 		setEnergyData(1, 2, 1, 2, 4);
 	}
