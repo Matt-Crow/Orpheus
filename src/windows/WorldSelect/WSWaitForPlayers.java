1608a74e5f94f4268b1fe4f0bd1ecd5b0489a137
IronHeart7334 <greengrappler12@gmail.com>
2019-06-17 14:00:18 -0700

    I have barely any idea what I'm doing

--- src/windows/WorldSelect/WSWaitForPlayers.java
+++ src/windows/WorldSelect/WSWaitForPlayers.java
@@ -1,14 +1,20 @@
 package windows.WorldSelect;
 
+import controllers.Master;
 import entities.Player;
 import java.util.ArrayList;
 import gui.Chat;
+import gui.Style;
 import java.awt.GridLayout;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JButton;
 import windows.Page;
 import windows.SubPage;
 
@@ -20,6 +26,10 @@ public class WSWaitForPlayers extends SubPage{
     private int teamSize;
     private ArrayList<Player> team1Players;
     private ArrayList<Player> team2Players;
+    private JButton joinT1Button;
+    private JButton joinT2Button;
+    private Thread serverListenerThread;
+    private Socket socket;
     
     public WSWaitForPlayers(Page p){
         super(p);
@@ -27,26 +37,40 @@ public class WSWaitForPlayers extends SubPage{
         teamSize = 1;
         team1Players = new ArrayList<>();
         team2Players = new ArrayList<>();
+        
+        joinT1Button = new JButton("Joint team 1");
+        joinT1Button.addActionListener((e)->{
+            joinTeam1(Master.TRUEPLAYER);
+        });
+        Style.applyStyling(joinT1Button);
+        add(joinT1Button);
+        
         Chat.addTo(this);
-        setLayout(new GridLayout(1, 1));
+        setLayout(new GridLayout(2, 1));
     }
     
     public WSWaitForPlayers startServer(){
-        Thread serverListener = new Thread(){
-            @Override
-            public void run(){
-                int port = 6066;
-                String server = "localhost";
-                try {
-                    ServerSocket servSock = new ServerSocket(port);
-                    Socket sock = servSock.accept();
-                    sock.close();
-                } catch (IOException ex) {
-                    Chat.log(ex.getMessage());
+        if(serverListenerThread == null){
+            serverListenerThread = new Thread(){
+                @Override
+                public void run(){
+                    int port = 6066;
+                    String server = "localhost";
+                    try {
+                        ServerSocket servSock = new ServerSocket(port);
+                        socket = servSock.accept();
+                        new DataOutputStream(socket.getOutputStream()).writeUTF("Hello, is this thing on?");
+                        while(!socket.isInputShutdown()){
+                            System.out.println(new DataInputStream(socket.getInputStream()).readUTF());
+                        }
+                        socket.close();
+                    } catch (IOException ex) {
+                        Chat.log(ex.getMessage());
+                    }
                 }
-            }
-        };
-        serverListener.start();
+            };
+            serverListenerThread.start();
+        }
         
         return this;
     }
@@ -66,6 +90,13 @@ public class WSWaitForPlayers extends SubPage{
         }else if(team1Players.size() < teamSize){
             team1Players.add(p);
             Chat.log(p.getName() + " has joined team 1.");
+            if(socket != null){
+                try {
+                    new ObjectOutputStream(socket.getOutputStream()).writeObject(team1Players);
+                } catch (IOException ex) {
+                    Logger.getLogger(WSWaitForPlayers.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         }else{
             Chat.log(p.getName() + " cannot joint team 1: Team 1 is full.");
         } 
@@ -82,6 +113,13 @@ public class WSWaitForPlayers extends SubPage{
         }else if(team2Players.size() < teamSize){
             team2Players.add(p);
             Chat.log(p.getName() + " has joined team 2.");
+            if(socket != null){
+                try {
+                    new ObjectOutputStream(socket.getOutputStream()).writeObject(team2Players);
+                } catch (IOException ex) {
+                    Logger.getLogger(WSWaitForPlayers.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         }else{
             Chat.log(p.getName() + " cannot joint team 2: Team 2 is full.");
         } 
