4b0e82f36fb9e835b08027af0a0379410a60f438
IronHeart7334 <greengrappler12@gmail.com>
2019-07-04 08:58:48 -0700

    Oh. I could just do that

--- src/net/Connection.java
+++ src/net/Connection.java
@@ -19,31 +19,46 @@ import java.util.logging.Logger;
 public class Connection {
     private User user; //the user playing Orpheus on the machine this connects to
     private final Socket clientSocket;
-    private final DataInputStream ip;
-    private final DataOutputStream op;
+    //private final DataInputStream ip;
+    //private final DataOutputStream op;
     //not sure if I need both
-    //private final ObjectOutputStream objOut;
-    //private final ObjectInputStream objIn;
+    private final ObjectOutputStream objOut;
+    private final ObjectInputStream objIn;
+    
+    private static final String END_MSG = "END OF LINE.";
     
     public Connection(Socket s) throws IOException{
         clientSocket = s;
-        ip = new DataInputStream(new BufferedInputStream(s.getInputStream()));
-        op = new DataOutputStream(new BufferedOutputStream(s.getOutputStream()));
-        //objIn = new ObjectInputStream(s.getInputStream());
-        //objOut = new ObjectOutputStream(s.getOutputStream());
+        //ip = new DataInputStream(new BufferedInputStream(s.getInputStream()));
+        //op = new DataOutputStream(new BufferedOutputStream(s.getOutputStream()));
+        objIn = new ObjectInputStream(s.getInputStream());
+        objOut = new ObjectOutputStream(s.getOutputStream());
     }
     
     public void writeToClient(String s) throws IOException{
+        objOut.writeObject(s);
+        objOut.flush();
+        /*
+        int strBytes = s.length() * 2; //2 bytes per character
+        //can only write up to 512 bytes at a time using writeUTF,
+        //and 3 are reserved for marking the beginning and end of message
+        
+        if(strBytes > 512 - 3){
+            for(int start = 0; start + 509 < strBytes; start += strBytes){
+                op.writeUTF(s.substring(start / 2, (start + strBytes) / 2));
+            }
+        }
         op.writeUTF(s);
-        op.flush();
+        op.flush();*/
     }
     /*
     public void writeToClient(Object o) throws IOException{
         objOut.writeObject(o);
     }*/
     
-    public String readFromClient() throws IOException{
-        return ip.readUTF();
+    public String readFromClient() throws IOException, ClassNotFoundException{
+        //return ip.readUTF();
+        return (String)objIn.readObject();
     }
     /*
     public Object readObjectFromClient() throws IOException, ClassNotFoundException{
@@ -52,15 +67,15 @@ public class Connection {
     
     public void close(){
         try {
-            op.close();
-            //objOut.close();
+            //op.close();
+            objOut.close();
         } catch (IOException ex) {
             System.err.println("couldn't close output");
             ex.printStackTrace();
         }
         try {
-            ip.close();
-            //objIn.close();
+            //ip.close();
+            objIn.close();
         } catch (IOException ex) {
             System.err.println("couldn't close input");
             ex.printStackTrace();
