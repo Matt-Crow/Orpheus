2f05bb85a535521dd6c088820e320962c7aafa81
IronHeart7334 <greengrappler12@gmail.com>
2019-07-09 09:37:47 -0700

    something about deadlock detected

--- src/controllers/World.java
+++ src/controllers/World.java
@@ -103,7 +103,7 @@ public class World implements Serializable{
     
     public final World addTeam(Team t){
         teams.put(t.getId(), t);
-        //t.forEachMember((Player p)->p.setWorld(this));
+        t.forEachMember((Player p)->p.setWorld(this));
         t.forEach((Entity e)->e.setWorld(this));
         return this;
     }
