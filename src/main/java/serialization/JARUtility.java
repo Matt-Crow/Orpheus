c82776ff7ff09085a418144847fde7dc85e77e6d
IronHeart7334 <greengrappler12@gmail.com>
2020-02-17 09:23:47 -0800

    Well, creating data sets works, but it is a bit clunky

--- src/main/java/serialization/JARUtility.java
+++ src/main/java/serialization/JARUtility.java
@@ -1,5 +1,7 @@
 package serialization;
 
+import gui.FileChooserUtil;
+import java.io.File;
 import static java.lang.System.out;
 import java.util.Scanner;
 
@@ -12,12 +14,41 @@ public class JARUtility {
         Scanner in = new Scanner(System.in);
         int ip = 0;
         do {
-            out.println("Choose an option:");
+            out.println("***JAR UTILITY***");
             out.println("1: Create a new set of customizables");
             out.println("-1: Quit");
+            out.println("Choose an option: ");
             ip = in.nextInt();
+            switch(ip){
+                case 1:
+                    FileChooserUtil.chooseDir("Where do you want to create the new data set?", (f)->{
+                        in.nextLine();
+                        out.println("Enter a name for this data set: ");
+                        String name = in.nextLine();
+                        createNewDataSet(f, name);
+                    });
+                    break;
+            }
+            in.nextLine();
         } while(ip != -1);
         in.close();
+    }
+    
+    public static File createNewDataSet(File parentDir, String name){
+        File newDir = new File(parentDir + File.separator + name);
+        if(newDir.mkdir()){
+            File mainFolder = new File(newDir.getAbsolutePath() + File.separator + "customizables");
+            if(mainFolder.mkdir()){
+                new File(mainFolder.getAbsolutePath() + File.separator + "actives").mkdir();
+                new File(mainFolder.getAbsolutePath() + File.separator + "characterClass").mkdir();
+                new File(mainFolder.getAbsolutePath() + File.separator + "passives").mkdir();
+            } else {
+                System.err.println("Failed to create " + mainFolder.getAbsolutePath());
+            }
+        } else {
+            System.err.println("Failed to create " + newDir.getAbsolutePath());
+        }
+        return newDir;
     }
     
     public static void main(String[] args){
