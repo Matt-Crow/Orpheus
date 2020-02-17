10debd2617204f8e1725b4242319bfcc1aab191d
IronHeart7334 <greengrappler12@gmail.com>
2020-02-17 13:42:08 -0800

    Thats it. I'm done. Sick of this nofnde.bn gnkojhg

--- src/main/java/serialization/JARUtility.java
+++ src/main/java/serialization/JARUtility.java
@@ -2,74 +2,23 @@ package serialization;
 
 import gui.FileChooserUtil;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import static java.lang.System.out;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Locale;
 import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.JavaFileObject.Kind;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.StandardJavaFileManager;
 import javax.tools.ToolProvider;
 
 /**
  *
  * @author Matt Crow
  */
-public class JARUtility {
-    public static void run(){
-        Scanner in = new Scanner(System.in);
-        int ip = 0;
-        do {
-            out.println("***JAR UTILITY***");
-            out.println("1: Create a new data set of customizables");
-            out.println("2: Create a JAR file for a data set");
-            out.println("-1: Quit");
-            out.println("Choose an option: ");
-            ip = in.nextInt();
-            switch(ip){
-                case 1:
-                    FileChooserUtil.chooseDir("Where do you want to create the new data set?", (f)->{
-                        in.nextLine();
-                        out.println("Enter a name for this data set: ");
-                        String name = in.nextLine();
-                        createNewDataSet(f, name);
-                    });
-                    break;
-                case 2:
-                    try {
-                        compile();
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
-                    break;
-            }
-            in.nextLine();
-        } while(ip != -1);
-        in.close();
-    }
-    
-    public static File createNewDataSet(File parentDir, String name){
-        File newDir = new File(parentDir + File.separator + name);
-        if(newDir.mkdir()){
-            File mainFolder = new File(newDir.getAbsolutePath() + File.separator + "customizables");
-            if(mainFolder.mkdir()){
-                new File(mainFolder.getAbsolutePath() + File.separator + "actives").mkdir();
-                new File(mainFolder.getAbsolutePath() + File.separator + "characterClass").mkdir();
-                new File(mainFolder.getAbsolutePath() + File.separator + "passives").mkdir();
-            } else {
-                System.err.println("Failed to create " + mainFolder.getAbsolutePath());
-            }
-        } else {
-            System.err.println("Failed to create " + newDir.getAbsolutePath());
-        }
-        return newDir;
-    }
-    
+public class JARUtility {    
     public static void compile() throws IOException{
         FileChooserUtil.chooseDir("Select the directory to compile", (File compileMe)->{
             FileChooserUtil.chooseDir("Choose directory to send compiled files to", (File outputDir)->{
@@ -91,21 +40,25 @@ public class JARUtility {
                     compile(javac, child, outputDir, orpheusPath);
                 }
             } else {
-                String className = compileMe.getName().replace(".java", ".class");
-                Path newPath = Paths.get(outputDir.getAbsolutePath(), className);
-                //javac.run(new FileInputStream(compileMe), new FileOutputStream(newPath.toFile()), System.err, "classpath=" + orpheusPath);
+                //https://stackoverflow.com/questions/53187175/compile-java-file-at-runtime
+                StandardJavaFileManager fileManager = javac.getStandardFileManager(null, Locale.ENGLISH, Charset.defaultCharset());
+                ArrayList<String> options = new ArrayList<>();
+                ArrayList<JavaFileObject> files = new ArrayList<>();
+                files.add(new SimpleJavaFileObject(compileMe.toURI(), Kind.SOURCE));
+                javac.getTask(null, fileManager, null, options, null, files);
+                /*
                 javac.run(
-                    System.in, 
-                    System.out, 
-                    System.err, 
+                    null,
+                    null,
+                    null,
                     new String[]{String.format("\"%s\"", compileMe.getAbsolutePath())}
                     //String.format(" -d \"%s\" -classpath \"%s\" \"%s\"", outputDir.getAbsolutePath(), orpheusPath, compileMe.getAbsolutePath())
-                );
+                );*/
             }
         }
     }
     
-    public static void main(String[] args){
-        run();
+    public static void main(String[] args) throws IOException{
+        compile();
     }
 }
