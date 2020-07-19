ac0b64fff354427cc19139c82e0dc9c80effbb97
IronHeart7334 <greengrappler12@gmail.com>
2020-07-19 15:03:57 -0700

    forgot this

---
+++ src/main/java/net/protocols/AbstractOrpheusServerProtocol.java
@@ -0,0 +1,30 @@
+package net.protocols;
+
+import java.io.IOException;
+import net.OrpheusServer;
+import net.ServerMessage;
+
+/**
+ *
+ * @author Matt
+ */
+public abstract class AbstractOrpheusServerProtocol {
+    public abstract void applyProtocol() throws IOException;
+    
+    /**
+     * An instance of OrpheusServer should pass itself as a parameter to this function
+     * upon receiving a server message.
+     * 
+     * @param sm the message received
+     * @param forServer the server which received this message
+     * @return whether or not this method handled the message.
+     */
+    public abstract boolean receiveMessage(ServerMessage sm, OrpheusServer forServer);
+    
+    /**
+     * Called whenever applyProtocol() is
+     * invoked. This should reset the protocol,
+     * preparing it.
+     */
+    public abstract void doApplyProtocol();
+}
