7ab39808d6fb82daed354d534e886738eba913de
IronHeart7334 <greengrappler12@gmail.com>
2020-07-29 14:34:24 -0700

    132 files... At this point, I think I want to just move multiplayer out of this, and into a simpler program.

---
+++ countClasses.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import os
+
+def countFiles(pathStr):
+    pathObj = Path(pathStr)
+    print("Count files for " + str(pathObj))
+
+    total = 0
+    for fileOrDir in pathObj.iterdir():
+        print(fileOrDir)
+        if fileOrDir.is_dir():
+            total = total + countFiles(fileOrDir)
+        else:
+            total = total + 1
+    print(str(pathObj) + " contains " + str(total) + " files")
+    return total
+
+if __name__ == "__main__":
+    root = os.path.join(os.getcwd(), "src", "main", "java")
+    print(root)
+    print(countFiles(root))
